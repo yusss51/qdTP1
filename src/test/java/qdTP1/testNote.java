efefb24c50b9956ee17f71dff372122bf611e514
yusdel5 <113824015+yusdel5@users.noreply.github.com>
2023-02-20 01:07:03 +0100

    Test

--- src/test/java/qdTP1/testNote.java
+++ src/test/java/qdTP1/testNote.java
@@ -1,11 +1,110 @@
 package qdTP1;
 
+import calcultableau.Note;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class testNote {
 
 
 
+    Note noteTest;
+
+    @BeforeAll
+    public static void init() {
+
+        ArrayList <Integer> list = new ArrayList<Integer>();
+
+
+        list.add(20);
+        list.add(20);
+        list.add(10);
+        list.add(20);
+
+        Note note = new Note("17/02/2023", list);
+    }
+
+
+    /**
+     On test la moyenne
+     */
+    @Test
+    void testMoyenneInvalide() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(20);
+        list.add(1000);
+        list.add(20);
+        list.add(10);
+        list.add(20);
+
+        list.add(-1000);
+        Note noteTest = new Note("17/02/2023", list);
+
+
+        assertEquals(noteTest.moyenne(), 10);
+    }
+
+    /**
+     On test pour savoir si les notes sont bien comprises entre 0 et 20
+     */
+    @Test
+    void testZeroVingt() {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+        list.add(20);
+        list.add(1000);
+        list.add(20);
+
+        list.add(10);
+        list.add(20);
+        list.add(-1000);
+
+        boolean isInvalide = false;
+        for (Integer val : list) {
+            if (val > 20 || val < 0) {
+                isInvalide = true;
+                break;
+            }
+        }
+        assertTrue(isInvalide);
+    }
+
+
+    /**
+     On test la mediane
+     */
+    @Test
+    void testMedian() {
+
+
+        ArrayList <Integer> listt = noteTest.getNotes();
+
+
+        ArrayList <Integer> list = new ArrayList<Integer>();
+
+
+        list.add(10);
+        list.add(10);
+        list.add(10);
+        list.add(15);
+
+        list.add(20);
+        list.add(20);
+        list.add(20);
+
+        noteTest.setNotes(list);
+        assertEquals(noteTest.median(), 20);
+
+        noteTest.setNotes(listt);
+
+    }
 
 
 
--- src/test/java/qdTP1/testUtilisateur.java
+++ src/test/java/qdTP1/testUtilisateur.java
@@ -1,8 +1,80 @@
 package qdTP1;
 
+import calcultableau.Utilisateur;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class testUtilisateur {
+
+    @BeforeAll
+    public static void init() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+    }
+
+    @Test
+    void test() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+        assertEquals(enseignant.getPrenom(), "Alain");
+    }
+
+    /**
+     On test si le nom et le prenom contiennent au moins un carcatere
+     */
+    @Test
+    void testCaractereNomPrenom() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+        String nom = enseignant.getNom();
+        String prenom = enseignant.getPrenom();
+
+
+
+        assertThat(nom).isNotEmpty();
+        assertThat(prenom).isNotEmpty();
+
+    }
+
+    /**
+     On test si le nom et le prenom contiennent que des lettres
+     */
+    @Test
+    void testLettresNomPrenom() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+        String nom = enseignant.getNom();
+        String prenom = enseignant.getPrenom();
+
+
+
+        assertThat(nom).contains("Dupont");
+        assertThat(prenom).contains("Alain");
+        assertThat(nom).doesNotContain("1").doesNotContain("2").doesNotContain("3").doesNotContain("4")
+                .doesNotContain("5").doesNotContain("6").doesNotContain("7").doesNotContain("8").doesNotContain("9")
+                .doesNotContain("0");
+        assertThat(prenom).doesNotContain("1").doesNotContain("2").doesNotContain("3").doesNotContain("4")
+                .doesNotContain("5").doesNotContain("6").doesNotContain("7").doesNotContain("8").doesNotContain("9")
+                .doesNotContain("0");
+    }
+
+    /**
+     On test l'email de  l'utilisateur
+     */
+    @Test
+    void testEmail() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+        assertEquals(enseignant.getMail(), "alain.dupont@iut.fr");
+    }
+
+    /*
+    On test le nom de l'utilisateur
+     */
+
+    @Test
+    void testNom() {
+        Utilisateur enseignant = new Utilisateur("Alain", "Dupont", "alain.dupont@iut.fr");
+        assertEquals(enseignant.getNom(), "Dupont");
+    }
 
 
 
